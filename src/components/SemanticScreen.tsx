439f89b726d164cf61880aa9b937cc9379aba2f5
Joseph Turner <joseph@breatheoutbreathe.in>
2020-08-02 20:02:11 -0700

    resolved bug when clicking on focus region when no focus point exists.

--- src/components/SemanticScreen.tsx
+++ src/components/SemanticScreen.tsx
@@ -90,6 +90,8 @@ const SemanticScreen = (props: {
       deleteEmptyPoints();
       if (region === "focus" && message.focus) {
         props.setEditingPoint(message.focus.pointId);
+      } else if (region === "focus") {
+        return;
       } else if (region === "merits") {
         console.log("merits clicked");
       } else if (!childClicked) {
@@ -98,10 +100,8 @@ const SemanticScreen = (props: {
             author: author,
             content: "",
           },
-
-          // TODO: type error here, region can include "focus", but a point cannot have shape "focus"
-          shape: region as PointShape,
-          index: message.points[region as PointShape].length,
+          shape: region,
+          index: message.points[region].length,
         });
       }
     } else if (region === expandedRegion && !childClicked) {
