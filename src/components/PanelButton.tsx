7520a9f68939544dc507a2a1123097b0bb9c46c7
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-10-11 21:09:01 -0700

    Removed fill-rule from SVG in PanelButton component.

--- src/components/PanelButton.tsx
+++ src/components/PanelButton.tsx
@@ -22,25 +22,21 @@ const PanelButton = (props: PanelButtonProps) => (
   >
     {props.side === "right" && props.openClose === "open" && (
       <path
-        fill-rule="evenodd"
         d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"
       />
     )}
     {props.side === "right" && props.openClose === "close" && (
       <path
-        fill-rule="evenodd"
         d="M9.224 1.553a.5.5 0 0 1 .223.67L6.56 8l2.888 5.776a.5.5 0 1 1-.894.448l-3-6a.5.5 0 0 1 0-.448l3-6a.5.5 0 0 1 .67-.223z"
       />
     )}
     {props.side === "bottom" && props.openClose === "open" && (
       <path
-        fill-rule="evenodd"
         d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"
       />
     )}
     {props.side === "bottom" && props.openClose === "close" && (
       <path
-        fill-rule="evenodd"
         d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"
       />
     )}
